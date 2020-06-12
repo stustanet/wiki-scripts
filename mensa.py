190fccedcbf91b97b534798b5d4835957e2a84ae
Julien Schmidt <js@stusta.net>
2020-06-12 03:30:40 +0200

    mensa: port to py3 and style fixes

--- mensa.py
+++ mensa.py
@@ -1,55 +1,61 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import mwclient, ConfigParser
-import urllib2
-import cgi
+import configparser
+import html
+import sys
+import urllib.request
 from datetime import date
-from BeautifulSoup import BeautifulSoup
 
-config = ConfigParser.RawConfigParser()
-config.read('mensa.ini')
-cfg = config.get
+import mwclient
+from bs4 import BeautifulSoup
 
 
-speisen = {}
+def main():
+    config = configparser.RawConfigParser()
+    config.read('mensa.ini')
+    cfg = config.get
 
-proxy_handler = urllib2.ProxyHandler({'http': cfg('mensa', 'proxy')})
-opener = urllib2.build_opener(proxy_handler)
-f = opener.open(cfg('mensa', 'url'))
+    speisen = {}
 
-page = f.read()
-# hack to make python parser happy
-page = page.replace("</scri'+'pt>","</script>")
+    proxy_handler = urllib.request.ProxyHandler(
+        {'http': cfg('mensa', 'proxy')})
+    opener = urllib.request.build_opener(proxy_handler)
+    page = opener.open(cfg('mensa', 'url')).read()
+    soup = BeautifulSoup(page, 'html.parser')
 
-a = BeautifulSoup(page)
+    try:
+        today = soup.find(attrs={'class': 'heute_' + date.today().strftime(
+            '%Y_%m_%d') + '  anker'}).findPrevious(attrs={'class': 'menu'})
+        rows = today.findAll('tr')
+        for row in rows:
+            gericht = row.findAll('span', attrs={'class': 'stwm-artname'})
+            beschreibung = row.findAll('span')
+            if gericht and beschreibung:
+                gericht = gericht[0].string
+                beschreibung = beschreibung[1].string
+                speisen[gericht] = beschreibung
+    except Exception as ex:
+        print(ex)
+        speisen = None
 
-try:
-    today = a.find(attrs={'class' : 'heute_' + date.today().strftime('%Y-%m-%d') + ' anker'}).findPrevious(attrs={'class': 'menu'})
-    b = today.findAll('tr')
-    for x in b:
-        gericht = x.findAll('span', attrs={'class': 'stwm-artname'})
-        beschreibung = x.findAll('span')
-        if gericht and beschreibung:
-            gericht = gericht[0].string
-            beschreibung = beschreibung[1].string
-            speisen[gericht] = beschreibung
-except:
-    speisen = None
+    site = mwclient.Site(cfg(
+        'mwclient', 'site'), path=cfg('mwclient', 'path'))
+    site.login(cfg('mwclient', 'user'), cfg('mwclient', 'pass'))
+
+    page = site.Pages['Vorlage:Mensa-Heute']
+
+    text = ''
+    if speisen:
+        for speise in speisen:
+            text = text + '%s: %s' % (html.escape(speise),
+                                      html.escape(speisen[speise]))
+            text = text + '<br />\n'
+    else:
+        text = text + 'Heute nix\n'
+
+    page.save(text, summary='Mensa Essen ' + date.today().strftime('%Y-%m-%d'))
 
 
-site = mwclient.Site((cfg('mwclient', 'schema'), cfg('mwclient', 'site')), path=cfg('mwclient', 'path'))
-site.login(cfg('mwclient', 'user'), cfg('mwclient', 'pass'))
-
-page = site.Pages['Vorlage:Mensa-Heute']
-
-text = ''
-if speisen:
-    for x in speisen:
-        text = text + '%s: %s' % (cgi.escape(x), cgi.escape(speisen[x]))
-        text = text + '<br />\n'
-else:
-    text = text + 'Heute nix\n'
-
-page.save(text, summary = 'Mensa Essen ' + date.today().strftime('%Y-%m-%d'))
-
+if __name__ == '__main__':
+    sys.exit(main())
