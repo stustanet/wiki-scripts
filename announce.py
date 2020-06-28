86efabe9e727d1ded8b636b9b194d17f68e362f9
Jan Fecht <jan.fecht@tum.de>
2020-06-28 16:13:37 +0200

    announce: Also send ical event

--- announce.py
+++ announce.py
@@ -15,14 +15,22 @@
 # Complete rewrite in python3 using the API, 04/2018
 #     J. Schmidt <js@stusta.net>
 
+import re
 import smtplib
 import sys
 import urllib.parse
+from datetime import datetime, timedelta
 from email.message import EmailMessage
 from email.utils import localtime
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email import encoders
 
 import mwclient
+import pytz
 from bs4 import BeautifulSoup
+from ics import Event, Calendar
 
 
 def format_news(entry, page):
@@ -54,8 +62,37 @@ def format_news(entry, page):
                 body += "\n"
             if location != "":
                 body += "Ort: " + location + "\n"
+
         else:
             text += item + "\n"
+
+    calendar = None
+    try:
+        tz = pytz.timezone('Europe/Berlin')
+        starttime = tz.localize(datetime.strptime(start, "%Y/%m/%d %H:%M:%S"))
+        event = Event()
+        event.name = entry['Titel']
+        event.begin = starttime
+        try:
+            endtime = tz.localize(datetime.strptime(end, "%Y/%m/%d %H:%M:%S"))
+        except ValueError:
+            # default to 2 hours duration
+            endtime = starttime + timedelta(hours=2)
+        event.end = endtime
+        if location != "":
+            event.location = location
+        text = page.text()
+        ms = [re.search(r"\|Zusammenfassung=(.*)", line)
+              for line in text.split('\n')]
+        ms = [m for m in ms if m is not None]
+        if len(ms) > 0:
+            event.description = ms[0].groups()[0]
+
+        calendar = Calendar()
+        calendar.events.add(event)
+    except ValueError:
+        print(f"Value error: couldn't parse date {start}")
+        calendar = None
 
     body += "Zusammenfassung:\n"
     body += entry['Zusammenfassung']
@@ -69,21 +106,40 @@ def format_news(entry, page):
     body += "Quelle: https://wiki.stusta.de/" + \
         urllib.parse.quote(entry['Page'].replace(" ", "_"))
     body += "\n\n-- \nMehr Informationen: https://info.stusta.de\n"
-    return body
+    return (body, calendar)
+
+
+def attach_calendar(msg, calendar):
+    ical_atch = MIMEText('text', 'calendar', 'utf-8')
+    ical_atch.set_payload(str(calendar).encode("utf-8"), 'utf-8')
+    ical_atch.add_header('Content-Disposition',
+                         'attachment; filename="invite.ics"')
+    msg.attach(ical_atch)
 
 
 # send mail to announce list
-def send_mail(subject, author, body):
-    msg = EmailMessage()
-    msg.set_content(body)
+def send_mail(subject, author, body, calendar=None):
+    from_addr = "no-reply@mail.stusta.de"
+    to_addr = "announce@stusta.de"
+    if calendar:
+        msg = MIMEMultipart()
+        msg['date'] = str(localtime())
+    else:
+        msg = EmailMessage()
+        msg['date'] = localtime()
     msg['subject'] = subject
-    msg['date'] = localtime()
-    msg['from'] = author + " <no-reply@mail.stusta.de>"
-    msg['to'] = "announce@lists.stusta.de"
+    msg['from'] = author + f" <{from_addr}>"
+    msg['to'] = to_addr
     msg['reply-to'] = "StuStaNet e. V. Admins <admins@lists.stusta.de>"
 
+    if calendar:
+        msg.attach(MIMEText(body, 'plain'))
+        attach_calendar(msg, calendar)
+    else:
+        msg.set_content(body)
+
     smtp = smtplib.SMTP('mail.stusta.de')
-    smtp.send_message(msg)
+    smtp.sendmail(from_addr, to_addr, msg.as_string())
     smtp.quit()
 
 
@@ -104,7 +160,7 @@ def main():
         if author == "":
             author = "Infoseite"
         subject = entry['Titel']
-        send_mail(subject, author, format_news(
+        send_mail(subject, author, *format_news(
             entry, site.pages[entry['Page']]))
 
 
